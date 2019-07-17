ae2f68e1803e8a65829ab79b6076cf17e1e73ed8
MonikaNizio <46495323+MonikaNizio@users.noreply.github.com>
2019-07-17 14:19:44 +0100

    PCA

--- sound_analysis/load_sound.py
+++ sound_analysis/load_sound.py
@@ -1,5 +1,20 @@
 from scipy.io.wavfile import read
-import numpy
+from librosa.core import stft
+import numpy as np
+from sklearn.decomposition import PCA
+
 a = read("wind_01.wav")
-a = numpy.array(a[1],dtype=float)
+a = np.array(a[1],dtype=float)
 print(a)
+A = np.abs(stft(a))
+print(A)
+
+pca = PCA(n_components=2)
+#pca.fit(A)
+
+#print pca.explained_variance_ratio_
+
+
+pca.fit_transform(A)
+
+print ("A", A)
