634866240a401c59ded3b48d48623fb758c2ce4d
MonikaNizio <46495323+MonikaNizio@users.noreply.github.com>
2019-07-29 22:58:38 +0100

    Sound samples

---
+++ sound_analysis/bayesian.py
@@ -0,0 +1,10 @@
+### General imports
+%matplotlib inline
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import colors as mcolors
+from load_sound import
+
+### --- Figure config
+LEGEND_SIZE = 15
+
--- sound_analysis/load_sound.py
+++ sound_analysis/load_sound.py
@@ -3,18 +3,22 @@ from librosa.core import stft
 import numpy as np
 from sklearn.decomposition import PCA
 
-a = read("wind_01.wav")
-a = np.array(a[1],dtype=float)
-print(a)
-A = np.abs(stft(a))
-print(A)
+#class ProcessAudio:
+def audio_to_vector(audio):
+    #self.audio = audio
+    a = read(audio)
+    a = np.array(a[1],dtype=float)
+    #print(a)
+    A = np.abs(stft(a))
+    #print(A)
 
-pca = PCA(n_components=2)
-#pca.fit(A)
+    pca = PCA(n_components=8)
 
-#print pca.explained_variance_ratio_
+    pca.fit_transform(A)
 
+    print ("A", A)
+    return A
 
-pca.fit_transform(A)
-
-print ("A", A)
+syn1, syn2, syn3, syn4, syn5, syn6, syn7, syn8, syn9 = "synth_samples/synth1.wav", "synth_samples/synth2.wav", "synth_samples/synth3.wav", "synth_samples/synth4.wav", "synth_samples/synth5.wav", "synth_samples/synth6.wav", "synth_samples/synth7.wav", "synth_samples/synth8.wav", "synth_samples/synth9.wav"
+#syn1 = "synth_samples/synth1.wav"
+sample1 = audio_to_vector(syn1)
