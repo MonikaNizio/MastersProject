138ce79c60d9171a875b3e9c39b12a0712fb481d
Monika Nizio <2424127N@student.gla.ac.uk>
2019-07-30 11:44:23 +0100

    Added synth calues

--- sound_analysis/load_sound.py
+++ sound_analysis/load_sound.py
@@ -4,7 +4,7 @@ import numpy as np
 from sklearn.decomposition import PCA
 
 #class ProcessAudio:
-def audio_to_vector(audio):
+def audio_to_array(audio):
     #self.audio = audio
 
     #read the audio sample
@@ -23,9 +23,14 @@ def audio_to_vector(audio):
 
     pca.fit_transform(A)
 
-    print ("A", A)
+    #print ("A", A)
     return A
 
 syn1, syn2, syn3, syn4, syn5, syn6, syn7, syn8, syn9 = "synth_samples/synth1.wav", "synth_samples/synth2.wav", "synth_samples/synth3.wav", "synth_samples/synth4.wav", "synth_samples/synth5.wav", "synth_samples/synth6.wav", "synth_samples/synth7.wav", "synth_samples/synth8.wav", "synth_samples/synth9.wav"
 #syn1 = "synth_samples/synth1.wav"
-sample1 = audio_to_vector(syn1)
+#sample1 = audio_to_array(syn1)
+
+samples_arrays = [audio_to_array(syn1), audio_to_array(syn2), audio_to_array(syn3), audio_to_array(syn4), audio_to_array(syn5), audio_to_array(syn6), audio_to_array(syn7), audio_to_array(syn8), audio_to_array(syn9)]
+
+samples_values = [[127, 127, 127, 127, 127, 5999, 0, 0], [157, 127, 127, 127, 127, 5999, 0, 0], [127, 157, 127, 127, 127, 5999, 0, 0], [127, 127, 157, 127, 127, 5999, 0, 0], [127, 127, 127, 157, 127, 5999, 0, 0], [127, 127, 127, 127, 157, 5999, 0, 0], [127, 127, 127, 127, 127, 0, 0, 0], [127, 127, 127, 127, 127, 5999, 999, 0], [127, 127, 127, 127, 127, 5999, 0, 699]]
+
