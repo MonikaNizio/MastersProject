3cd671f55cf1499e580ae783fae7edf3e4a2bb77
Monika Nizio <2424127N@student.gla.ac.uk>
2019-09-06 07:26:09 +0100

    gui updated

--- sound_analysis/gui.py
+++ sound_analysis/gui.py
@@ -2,14 +2,7 @@ import tkinter as tk
 from tkinter import filedialog
 from bayesian import main_loop
 from osc_client import run_client
-# from osc_server import run_server
 from process_audio import audio_to_array
-
-#class GUI():
-
-# def open_file(self, event=None):
-#     self.filename = filedialog.askopenfilename()
-#     print('Selected:', self.filename)
 
 def open_file():
     root.filename = filedialog.askopenfilename(initialdir="C:/", title="Select a file", filetypes=(("wav files", ".wav"), ("All files", "*.*")))
@@ -26,13 +19,11 @@ root.geometry("700x500")
 root.title("Synth machine learning app")
 root.filename = "synth_samples/synth1.wav"
 
-instruction_label = tk.Label(root,text="Load in your sound and let the program choose the synth settings for you", font=("arial",14,"bold")).pack()
+instruction_label = tk.Label(root,text="Load in your sound (1s) and let the program choose the optimal synth settings", font=("arial",13,"bold")).pack()
 
 open_file_button = tk.Button(root, text='Open', command=open_file)
 open_file_button.pack()
 
 start_button = tk.Button(root, text='Start', command=run_optimization)
 start_button.pack()
-# window.filename = tk.filedialog.askopenfilename(initialdir = "C:\"",title = "Select file",filetypes = (("jpeg files","*.jpg"),("all files","*.*")))
-# print (window.filename)
 root.mainloop()
--- sound_analysis/osc_server.py
+++ sound_analysis/osc_server.py
@@ -14,96 +14,19 @@ def send_synth_values(*args):
     #print(args[i])
   print(args)
 
-# def record_audio():
-#   fs = 44100  # Sample rate
-#   seconds = 1  # Duration of recording
-#
-#   myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=1)
-#   sd.wait()  # Wait until recording is finished
-#   write('output.wav', fs, myrecording)  # Save as WAV file
+def run_server():
+  parser = argparse.ArgumentParser()
+  parser.add_argument("--ip",
+                            default="127.0.0.1", help="The ip to listen on")
+  parser.add_argument("--port",
+                            type=int, default=8000, help="The port to listen on")
+  args = parser.parse_args()
 
-#dispatcher = dispatcher.Dispatcher()
-#if __name__ == "__main__":
+  dispatcher = dispatcher.Dispatcher()
+  #print(getattr(dispatcher, "dispatcher"))
+  dispatcher.map("/synth_receive", send_synth_values)
 
-test_points = [143, 35, 72, 80, 29, 4488, 67, 429]
-
-#def run_server():
-# parser = argparse.ArgumentParser()
-# parser.add_argument("--ip",
-#                           default="127.0.0.1", help="The ip to listen on")
-# parser.add_argument("--port",
-#                           type=int, default=8000, help="The port to listen on")
-# args = parser.parse_args()
-#
-# dispatcher = dispatcher.Dispatcher()
-# #print(getattr(dispatcher, "dispatcher"))
-# dispatcher.map("/synth_receive", send_synth_values)
-#
-# server = osc_server.ThreadingOSCUDPServer(
-#       (args.ip, args.port), dispatcher)
-# print("Serving on {}".format(server.server_address))
-    #server.serve_forever()
-
-dispatcher = Dispatcher()
-#
-#
-# def set_filter(address: str, *args: List[Any]) -> None:
-#     # We expect two float arguments
-#     if not len(args) == 2 or type(args[0]) is not float or type(args[1]) is not float:
-#         return
-#
-#     # Check that address starts with filter
-#     if not address[:-1] == "/filter":  # Cut off the last character
-#         return
-#
-#     value1 = args[0]
-#     value2 = args[1]
-#     filterno = address[-1]
-#     print(f"Setting filter {filterno} values: {value1}, {value2}")
-#
-#
-# dispatcher.map("/filter*", set_filter)  # Map wildcard address to set_filter function
-# server = BlockingOSCUDPServer(("127.0.0.1", 8080), dispatcher)
-# client = SimpleUDPClient("127.0.0.1", 8080)
-#
-# # Send message and receive exactly one message (blocking)
-# client.send_message("/filter1", [1., 2.])
-# server.handle_request()
-#
-# client.send_message("/filter8", [6., -2.])
-# server.handle_request()
-
-'''
-dispatcher.map("/synth_receive", send_synth_values(test_points))  # Map wildcard address to set_filter function
-server = BlockingOSCUDPServer(("127.0.0.1", 8000), dispatcher)
-
-server.handle_request()
-
-
-#run_server()
-
-  # parser2 = argparse.ArgumentParser()
-  # parser2.add_argument("--ip",
-  #     default="127.0.0.1", help="The ip to listen on")
-  # parser2.add_argument("--port",
-  #     type=int, default=5050, help="The port to listen on")
-  # args2 = parser2.parse_args()
-  #
-  # dispatcher2 = dispatcher.Dispatcher()
-  # dispatcher2.map("/synth_receive", save_synth_values)
-  #
-  # server2 = osc_server.ThreadingOSCUDPServer(
-  #     (args.ip, args.port), dispatcher2)
-  # print("Serving on {}".format(server2.server_address))
-  # server2.serve_forever()
-
-
-# ser = serial.Serial('240')  # open serial port
-# print(ser.name)         # check which port was really used
-# ser.write(b'hello')     # write a string
-# ser.close()             # close port
-
-# with serial.Serial('a', 2400, timeout=1) as ser:
-#     x = ser.read()          # read one byte
-#     s = ser.read(10)        # read up to ten bytes (timeout)
-#     line = ser.readline()   # read a '\n' terminated line
+  server = osc_server.ThreadingOSCUDPServer(
+        (args.ip, args.port), dispatcher)
+  print("Serving on {}".format(server.server_address))
+  server.serve_forever()
